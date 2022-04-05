46fce084dd29d5679267f5f3dbf90499a8b221ed
prottay07 <prottay07@gmail.com>
2022-04-05 15:11:38 +0600

    Base Files including db

---
+++ database/migrations/2022_04_05_074125_create_seats_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('seats', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->unsignedBigInteger('bogi_id');
+            $table->unsignedBigInteger('train_id');
+            $table->unsignedBigInteger('type')->default(0)->comment('0=shovon, 1=singdha');
+            $table->unsignedBigInteger('booked')->default(0)->comment('0=open, 1=booked');          
+            $table->timestamps();
+
+            $table->foreign('bogi_id')->references('id')->on('bogis')->onDelete('cascade');
+            $table->foreign('train_id')->references('id')->on('trains');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('seats');
+    }
+};
